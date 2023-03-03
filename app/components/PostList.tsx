0210d1f6976eb4b71214676f6b64b08a8f57f932
Aleks Rutins <aleks@rutins.com>
2023-03-02 20:32:19 -0500

    More work on fetching from the DB

--- app/components/PostList.tsx
+++ app/components/PostList.tsx
@@ -3,5 +3,5 @@ import { mainDB } from "@/util/notion"
 export default async function PostList() {
     const db = await mainDB;
 
-    return <pre>{/*JSON.stringify(db, null, 2)*/}</pre>
+    return <pre>{JSON.stringify(db, null, 2)}</pre>
 }
--- util/notion.ts
+++ util/notion.ts
@@ -1,8 +1,28 @@
 import { NotionAPI } from "notion-client";
+import { Block } from "notion-types"
+
+type Post = {
+    name: string,
+    content: Block,
+    slug: string,
+    date: Date,
+    featured: boolean,
+    featuredImage?: string,
+    published: boolean,
+    category: string,
+    description: string,
+    image: string,
+    authors: string[],
+}
 
 export const notionAPI = new NotionAPI({authToken: process.env.NOTION_TOKEN_V2, activeUser: process.env.NOTION_USER_ID})
 export const mainDB = notionAPI.getPage(process.env.POSTS_TABLE_ID!);
 export async function posts() {
     const db = await mainDB;
     const blocks = Object.values(db.block);
+    const schema = Object.values(db.collection).find(c => c.value.name[0][0] == "Blog Pages")!.value.schema;
+    const fieldNames = new Map(
+        Object.keys(schema).map(key => [key, schema[key].name])
+    )
+    
 }
